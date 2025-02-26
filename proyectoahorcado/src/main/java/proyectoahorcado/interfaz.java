b113241277d3c753a7e5ce03e4ac6c35744fcf4b
Adri609 <adri.bog3@gmail.com>
2025-02-26 18:34:59 +0100

    inicioInterfaz
    Inicio del código de la interfaz

--- proyectoahorcado/src/main/java/proyectoahorcado/interfaz.java
+++ proyectoahorcado/src/main/java/proyectoahorcado/interfaz.java
@@ -12,7 +12,6 @@ public class Interfaz extends javax.swing.JFrame {
 
     private Logica ahorcado;
     final private Font fuente = new Font("Segoe print", Font.BOLD, 18);
-    JButton ingresar, reiniciar;
     private JLabel imagen;
     private JTextArea palabraSecreta, letrasUsadas, mensajeFinal;
     private JTextPane ingresarLetra;
@@ -26,8 +25,20 @@ public class Interfaz extends javax.swing.JFrame {
         //Se pide la palabra oculta
         JPasswordField palabraOculta = new JPasswordField();
         JOptionPane.showConfirmDialog(null, palabraOculta, "ingresa la palabra a adivinar:", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-
-
-
+        String palabra = new String(palabraOculta.getPassword());
+        ahorcado = new Logica(palabra);
+        
+        //Inicio todos los componentes gráficos (botones, etc...)
+        imagen = new JLabel(new ImageIcon("resources/ahorcado0.png"));
+        palabraSecreta = new JTextArea(ahorcado.getProgresoPalabra()); //Muestra el progreso inicial que sería la palabra ocultada
+        letrasUsadas = new JTextArea("Letras usadas:"); //Se muestran las letras utilizadas
+        mensajeFinal = new JTextArea(); //muestra mensaje de victoria o derrota
+        ingresarLetra = new JTextPane(); //Campo donde se meten las letras
+        JButton btnIngresar = new JButton("Ingresar"); //Botón para validar la palabra igresada
+        JButton btnReiniciar = new JButton("Reiniciar"); //Botón para reiniciar la partida
+        
+        //le ponemos acciones a los botones
+        btnIngresar.addActionListener(e -> );
+        
     }
 }
